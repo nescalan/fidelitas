b67a5ae6a0105d85c15f7f0d262bca64966d62a4
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2022-12-06 14:01:05 -0600

    Get In Touch Form: Styles

---
+++ 0-sandbox/udemy/PRACTICAS/formularios/get-in-touch/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS: Reset -->
+    <link rel="stylesheet" href="./css/reset.css">
+    <!-- CSS: Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Madurai&family=Hind:wght@400;700&display=swap"
+        rel="stylesheet">
+    <!-- CSS: Local -->
+    <link rel="stylesheet" href="./css/global.css">
+    <title>Form: Get in Touch</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-1">
+            <img src="./img/envelope.png" alt="envelope">
+        </div>
+        <div class="col-2">
+            <div class="card">
+                <h1>Get in touch</h1>
+                <form action="">
+                    <input class="name" type="text" placeholder="Name">
+                    <input class="email" type="email" placeholder="Email">
+                    <textarea id="message" class="message" name="message" cols="30" rows="5" placeholder="Message">
+                    </textarea>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
