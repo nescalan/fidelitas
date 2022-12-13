67b10f3e3fd1a5ff90034f17bba4813669e8030e
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2022-12-12 19:49:44 -0600

    new: Login y Registro de Usuarios

---
+++ 0-sandbox/udemy/PRACTICAS/login_registro_usuarios/views/contenido.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Contenido - Login</title>
+</head>
+
+<body>
+    <h1>Contenido del sitio</h1>
+    <a href="cerrar.php">Cerrar Session</a>
+    <article>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos in, dicta repudiandae nostrum voluptatum hic.
+            Fuga quibusdam, illum minima libero totam consectetur officiis consequatur amet deserunt quis, corrupti
+            adipisci repudiandae!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis labore esse tempore ratione exercitationem
+            reiciendis eveniet, ipsa inventore omnis quae veniam, accusantium officiis quis laboriosam. Cum eum
+            doloremque ea officiis?
+        </p>
+    </article>
+</body>
+
+</html>
