7cb631284c2dd86781f4c0f7c6798dbbb550997a
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2022-12-22 20:30:43 -0600

    update

---
+++ 0-sandbox/udemy/PRACTICAS/MySQL/practical-example/controler/login.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_hostname = "localhost";
+$db_database = "publications";
+$db_username = "nelson";
+$db_password = "4u3p7px6";
+
+?>
---
+++ 0-sandbox/udemy/PRACTICAS/MySQL/practical-example/mysqlitest.php
@@ -0,0 +1,9 @@
+<?php //mysqlitest.php
+
+require_once "./controler/connection.php";
+
+if ($connection) {
+    die("We are sorry, but is not possible to complete the requested task.");
+}
+
+?>
