71fa65c46c4113edb4afbb7d78a9c560a42625cd
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-02-14 14:14:14 -0600

    FEAT: JQuery
    + Selection by id and by class
    + Multiple elements selection
    + Descendants selectors
    + Selection by attribute

---
+++ 4-JQuery/Practicas de JQuery/ejercicio-3/1-seleccion-del-dom/por-id-y-clase/jq-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $("#container").html("Hola");
+
+  // Select the input field and change the placeholder attribute
+  $("#email").attr("placeholder", "Sorry can't continue your request");
+});
---
+++ 4-JQuery/Practicas de JQuery/ejercicio-3/1-seleccion-del-dom/seleccion-multiples-elementos/script.js
@@ -0,0 +1,3 @@
+$("document").ready(function () {
+  $(".class1").html("<h1> New text </h1x>");
+});
