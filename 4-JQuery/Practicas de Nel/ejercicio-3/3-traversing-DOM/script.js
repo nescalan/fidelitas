cf46ba0d934147cc046aef6023f93dc612776c12
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-02-15 10:46:07 -0600

    FEAT: traversing DOM

---
+++ 4-JQuery/Practicas de Nel/ejercicio-3/3-traversing-DOM/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // Metodo find()
+  let resultFind = $("#animals").find(".creature");
+  console.log("Resulst of the find() search: ");
+  console.log(resultFind);
+
+  // Metodo children()
+  let resultChildren = $("#animals").children();
+  console.log("Resulst of the Children search: ");
+  console.log(resultChildren);
+
+  // Metodos first(), last(), prev(), next()
+});
