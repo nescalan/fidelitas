902a0024e83438bd80357ec9bcc0ed516e8a520e
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-02-15 19:47:53 -0600

    update

---
+++ 4-JQuery/Practicas de Nel/curso-jquery/2-seleccion/por-id-y-clase/ejercicio-1/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+  setTimeout(function() {
+    // code to be executed after 3 seconds
+    $("section:first-child p").html(
+      "Hola Nelson, bienvenido a esta nueva aplicación."
+    );
+    $("section:last-child p").html(
+      "<p> El cambio hacia un futuro más sostenible es posible, pero requiere un compromiso global y un esfuerzo colectivo. Cada persona puede tomar medidas pequeñas en su vida diaria para reducir su huella de carbono y contribuir a solucionar el calentamiento global. </p>"
+    );
+  }, 5000);
+  
+
+
+});
+
