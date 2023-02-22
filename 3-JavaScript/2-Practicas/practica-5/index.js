ea08ed0ea2b5bbc38694b4ced8d0fc9863434354
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-02-21 19:38:07 -0600

    Update: jquery UI

---
+++ 3-JavaScript/2-Practicas/practica-5/index.js
@@ -0,0 +1,6 @@
+let firstName, lastName, age;
+
+firstName = prompt("Insert your first name: ");
+lastName = prompt("Insert your last name: ");
+age = prompt("Insert your age: ");
+alert(`El nombre ingresado es ${firstName} ${lastName} y tiene ${age} años`);
---
+++ 4-JQuery/jQuery UI/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("div.finderDirectory")
+    .mousedown(function () {
+      $("div.finderIconSelected").removeClass("finderIconSelected");
+
+      $("span.finderDirectoryNameSelected").removeClass(
+        "finderDirectoryNameSelected"
+      );
+
+      $(this).find("div.finderIcon").addClass("finderIconSelected");
+
+      $(this)
+        .find("div.finderDirectoryName span")
+        .addClass("finderDirectoryNameSelected");
+    })
+    .draggable();
+});
