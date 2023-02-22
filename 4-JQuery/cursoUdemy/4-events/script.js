ddd3fb34d4ce4f60e8d4c8b5d87343d25bfbe785
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-02-21 18:11:51 -0600

    update

--- 4-JQuery/cursoUdemy/4-events/script.js
+++ 4-JQuery/cursoUdemy/4-events/script.js
@@ -1,7 +1,18 @@
-// Eventos
 $(document).ready(function () {
+  // 15. Evento click
   $("#button").on("click", function () {
     // This: significa el objeto que llamamos  $("#button")
     $(this).toggleClass("highlighted");
+  });
+
+  // 16.- Ejemplo del evento click
+  $("#btnPick").on("click", function () {
+    let selected = $("#secSelector select option:selected");
+    let value = selected.val();
+    let price = selected.data("price");
+
+    if (price) {
+      $("#result").html(`El viaje a ${value} cuesta ${price}`);
+    }
   });
 });
