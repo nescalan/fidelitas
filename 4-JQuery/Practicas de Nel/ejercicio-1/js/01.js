a23e6f518cffd65703b04f63447d918e154d4a0b
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-02-23 19:28:17 -0600

    Practicas JQuery

--- 4-JQuery/Practicas de Nel/ejercicio-1/js/01.js
+++ 4-JQuery/Practicas de Nel/ejercicio-1/js/01.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
   $("poem-stanza").addClass("highlight");
+
+  // Hide elements
+  $(this).click(function () {
+    $(div).hide("#poem-stanza");
+  });
+
+  $("#hide").click(function () {
+    $(this).prev().hide();
+  });
 });
