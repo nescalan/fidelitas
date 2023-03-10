5a53ab427863a1da0e23262dfec832fabb67f18d
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-03-10 10:56:36 -0600

    FEAT: Shopping Cart Painting

--- 3-JavaScript/4-Proyecto/final-project/js/index.js
+++ 3-JavaScript/4-Proyecto/final-project/js/index.js
@@ -11,49 +11,49 @@ const priceList = [11.95, 14.95, 12.95, 15.95, 15.95, 13.95];
 
 let orderedPizzas = [];
 let orderedPrices = [];
-let firstProductName;
+let firstCartItem, secondCartItem;
+let rusticaPizza;
 
 // FUNCTIONS:
 function buyCheesePizza() {
-  orderedPizzas.push("Cheese Pizza");
+  orderedPizzas.push(pizzaOptions[0]);
   orderedPrices.push(priceList[0]);
+  firstCartItem = document.getElementById("1-product-name").innerText =
+    orderedPizzas[0];
+  // firstCartItem = document.getElementById("0-product-name").innerText =
+  //   orderedPizzas[0];
 
   console.log(orderedPizzas);
-
-  // alert(
-  //   `You just buy ${orderedPizzas[0]} and the price is $${orderedPrices[0]}`
-  // );
 }
+
 function buyPepperoniPizza() {
-  // orderedPizzas[0] = pizzaOptions[0];
   orderedPizzas.push(pizzaOptions[1]);
   orderedPrices.push(priceList[1]);
+  secondCartItem = document.getElementById("2-product-name").innerText =
+    orderedPizzas[1];
 
   console.log(orderedPizzas);
 }
+
 function buyVegetarianPizza() {
   orderedPizzas.push(pizzaOptions[2]);
-  orderedPrices.push(priceList[2]);
+  orderedPrices.push(priceList[3]);
+  secondCartItem = document.getElementById("3-product-name").innerText =
+    orderedPizzas[2];
 
   console.log(orderedPizzas);
-
-  // alert(
-  //   `You just buy ${orderedPizzas[0]} and the price is $${orderedPrices[0]}`
-  // );
 }
+
 function buyRusticaPizza() {
-  // orderedPizzas[0] = pizzaOptions[0];
   orderedPizzas.push(pizzaOptions[3]);
   orderedPrices.push(priceList[3]);
-
-  console.log(orderedPizzas);
+  rusticaPizza = document.getElementById("0-product-name").innerText =
+    orderedPizzas[0];
 }
 
-// Updating the DOM in Shopping Cart
-firstProductName = document.getElementById("0-product-name");
-firstProductName.innerText = orderedPizzas[1];
+function printShoppingCart(params) {}
 
-let quantity = document.getElementById("quantity");
-let pizzaQuantity = orderedPizzas.length;
-console.log(pizzaQuantity);
-quantity.innerText = pizzaQuantity;
+// Updating the DOM in Shopping Cart
+// const element = document.getElementById("product-name");
+// element.innerText = orderedPizzas[0];
+// console.log(element.innerText); // logs "New string"
