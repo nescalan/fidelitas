8c97250a76325cf781cabe45746645fee6be1404
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-03-10 11:55:14 -0600

    FEAT: Shopping Cart
    Note: pendding: Subtotal, taxes and total of invoice

--- 3-JavaScript/4-Proyecto/final-project/js/index.js
+++ 3-JavaScript/4-Proyecto/final-project/js/index.js
@@ -11,49 +11,84 @@ const priceList = [11.95, 14.95, 12.95, 15.95, 15.95, 13.95];
 
 let orderedPizzas = [];
 let orderedPrices = [];
-let firstCartItem, secondCartItem;
+let orderedImages = [];
+let shoppingCartOrders, cartPrice;
 let rusticaPizza;
 
 // FUNCTIONS:
 function buyCheesePizza() {
   orderedPizzas.push(pizzaOptions[0]);
   orderedPrices.push(priceList[0]);
-  firstCartItem = document.getElementById("1-product-name").innerText =
-    orderedPizzas[0];
-  // firstCartItem = document.getElementById("0-product-name").innerText =
-  //   orderedPizzas[0];
 
-  console.log(orderedPizzas);
+  printShoppingCart();
 }
 
 function buyPepperoniPizza() {
   orderedPizzas.push(pizzaOptions[1]);
   orderedPrices.push(priceList[1]);
-  secondCartItem = document.getElementById("2-product-name").innerText =
-    orderedPizzas[1];
-
-  console.log(orderedPizzas);
+  printShoppingCart();
 }
 
 function buyVegetarianPizza() {
   orderedPizzas.push(pizzaOptions[2]);
-  orderedPrices.push(priceList[3]);
-  secondCartItem = document.getElementById("3-product-name").innerText =
-    orderedPizzas[2];
-
-  console.log(orderedPizzas);
+  orderedPrices.push(priceList[2]);
+  printShoppingCart();
 }
 
 function buyRusticaPizza() {
   orderedPizzas.push(pizzaOptions[3]);
   orderedPrices.push(priceList[3]);
-  rusticaPizza = document.getElementById("0-product-name").innerText =
-    orderedPizzas[0];
+  printShoppingCart();
 }
 
-function printShoppingCart(params) {}
+function buyDeliciousPizza() {
+  orderedPizzas.push(pizzaOptions[4]);
+  orderedPrices.push(priceList[4]);
+  printShoppingCart();
+}
 
-// Updating the DOM in Shopping Cart
-// const element = document.getElementById("product-name");
-// element.innerText = orderedPizzas[0];
-// console.log(element.innerText); // logs "New string"
+function buyTomatoPizza() {
+  orderedPizzas.push(pizzaOptions[5]);
+  orderedPrices.push(priceList[5]);
+  printShoppingCart();
+}
+
+function printShoppingCart() {
+  for (let index = 0; index < orderedPizzas.length; index++) {
+    console.log(orderedPizzas[index]);
+    console.log(index);
+
+    switch (index) {
+      case 0:
+        shoppingCartOrders = document.getElementById(
+          "1-product-name"
+        ).innerText = orderedPizzas[index];
+        shoppingCartOrders = document.getElementById(
+          "1-product-price"
+        ).innerText = orderedPrices[index];
+
+        break;
+
+      case 1:
+        shoppingCartOrders = document.getElementById(
+          "2-product-name"
+        ).innerText = orderedPizzas[index];
+        shoppingCartOrders = document.getElementById(
+          "2-product-price"
+        ).innerText = orderedPrices[index];
+        break;
+
+      case 2:
+        shoppingCartOrders = document.getElementById(
+          "3-product-name"
+        ).innerText = orderedPizzas[index];
+        shoppingCartOrders = document.getElementById(
+          "3-product-price"
+        ).innerText = orderedPrices[index];
+        break;
+
+      default:
+        break;
+    }
+  }
+}
---
+++ 3-JavaScript/4-Proyecto/final-project/pruebas/pruebas.js
@@ -0,0 +1,13 @@
+array = [
+  "Cheese Pizza",
+  "Pepperoni Pizza",
+  "Vegetarian Pizza",
+  "Rustica Pizza",
+  "Delicious Pizza",
+  "Tomato Pizza",
+];
+
+for (let index = 0; index < array.length; index++) {
+  let element = array[index];
+  console.log(element);
+}
