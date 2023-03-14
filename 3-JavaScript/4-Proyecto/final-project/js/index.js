3f0d055dce033c131e7621c41b96f64be4cc2837
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-03-14 13:40:33 -0500

    FEAT: Refactoring all code

--- 3-JavaScript/4-Proyecto/final-project/js/index.js
+++ 3-JavaScript/4-Proyecto/final-project/js/index.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // FUNCTION: For Buying Pizzas
 function getProduct(index) {
-  alert(`getProduct ${index}`);
+  // alert(`getProduct ${index}`);
 
   if (orderedPizzas.includes(pizzaOptions[index])) {
     setPurchasedProduct();
@@ -52,10 +52,12 @@ function getProduct(index) {
 
 // ADD NEW CART
 function newCartItem() {
+  let contador;
+  let totalPrices = [];
+  var sum = 0;
   let cart = document.getElementById("cart");
   let quantityId = (document.getElementById("quantity").innerText =
     orderedPizzas.length);
-  let totalPrices = [];
   cart.innerHTML = "";
 
   if (orderedPizzas.length == 0) {
@@ -82,78 +84,59 @@ function newCartItem() {
           <div class="item-details">
             <h3 id="1-product-name">${orderedPizzas[i]}</h3>
             <p>Price: $<span id="1-product-price-${i}">${orderedPrices[i]}</span></p>
-            <p>Subtotal: $<span id="subtotal"> --</span></p>
-
+            <p>Subtotal: $<span id="subtotal${i}"> --</span></p>
             <input id="quantity-${i}" type="number" value="1" onchange="setUnitPrices(${i})" />
             <button class="remove" onclick="deleteCartItem(${i})">Remove</button>
           </div>
         </div>`;
       cart.innerHTML += cartItemTemplate;
-      // totalPrices.push(orderedPrices[i]);
+      totalPrices.push(orderedPrices[i]);
+    }
+
+    // Recorre el arreglo y suma los elementos
+    for (var i = 0; i < orderedPrices.length; i++) {
+      sum += orderedPrices[i];
     }
   }
 
-  setTotalPay(totalPrices);
+  setTotalPay(sum);
 }
 
 // ADD SHOPPING CART TOTAL
 function setTotalPay(prices) {
-  let total = 0;
-
-  // SUM
-  for (let i = 0; i < prices.length; i++) {
-    const value = prices[i];
-    total += value;
-  }
-
-  // TOFIXED: Set to decimal
-  let totalInvoice = total.toFixed(2);
   let orderSummary = document.getElementById("total-pay");
   let checkout = document.getElementById("checkout");
 
-  const totalPay = `
+  const domTotalPay = `
   <div class="cart-total">
-    <p>Total: $${totalInvoice}</p>
+    <p>Total: $${prices}</p>
   </div>
   `;
-  let checkoutInfo = `
+  let domCheckoutInfo = `
   <div class="checkout">
     <button>Checkout</button>
   </div>
   `;
 
   if (orderedPrices.length === 0) {
-    orderSummary.innerHTML = totalPay;
+    orderSummary.innerHTML = domTotalPay;
   } else {
-    orderSummary.innerHTML = totalPay;
-    checkout.innerHTML = checkoutInfo;
+    orderSummary.innerHTML = domTotalPay;
+    checkout.innerHTML = domCheckoutInfo;
   }
 }
 
 // FUNCTION CHANGE PRICE: Allows you to make individual price changes
 function setUnitPrices(index) {
-  let domProductQty = document.getElementById(`quantity-${index}`).value;
-
-  let domProductPrice = document.getElementById(
+  const domPrice = document.getElementById(
     `1-product-price-${index}`
   ).innerText;
+  const domQuantity = document.getElementById(`quantity-${index}`).value;
+  const domSubtotal = document.getElementById(`subtotal${index}`);
 
-  let subtotal = domProductPrice * domProductQty;
-
-  subtotal = subtotal.toFixed(2);
-
-  let domSubtotal = (document.getElementById("subtotal").innerText = subtotal);
-
-  console.log(
-    `domProductPrice: ${domProductPrice} | 
-    Cantidad: ${domProductQty} | 
-    Subtotal: ${subtotal} `
-  );
-
-  if (domProductQty < 1) {
-    console.log("La cantidad es menor a uno");
-    deleteCartItem(index);
-  }
+  let pricePerLine = domPrice * domQuantity;
+  pricePerLine = pricePerLine.toFixed(2);
+  domSubtotal.innerText = pricePerLine;
 }
 
 // FUNCTION: Delete Article
@@ -174,43 +157,3 @@ function setPurchase() {
   alert("Purchased");
   newCartItem();
 }
-
-// FUNCTION: suma de precios
-// function sumaPrecios(index) {
-//   let domProductQty = [];
-//   let result = [];
-//   let subtotal = [];
-//   let total = 0;
-//   let totalPrices = [];
-
-//   // DOM: Manipulation
-//   domProductQty[index] = document.getElementById(`quantity-${index}`).value;
-//   let domSubtotal = (document.getElementById("subtotal").innerText = subtotal);
-
-//   if (domProductQty < 1) {
-//     console.log("La cantidad es menor a uno");
-//     deleteCartItem(index);
-//   } else {
-//     console.log("---------------------------");
-//     // console.log(
-//     //   `Index: ${index} | Ordered Prices ${orderedPrices[index]} | length ${orderedPrices.length} `
-//     // );
-
-//     // Recorre el arreglo y suma los elementos
-//     for (var i = 0; i < orderedPrices.length; i++) {
-//       console.log(`Index: ${index} | Product Quantity:  ${domProductQty[index]}`);
-//       console.log(`Index: ${index} | Ordered Prices ${orderedPrices[index]}`);
-//       subtotal[index] = domProductQty[index] * orderedPrices[index];
-//       subtotal[index];
-//       console.log(`Index: ${index} | Subtotal ${subtotal[index]}`);
-//     }
-
-//     const numbers = [1.2345, 2.3456, 3.4567, 4.5678];
-
-//     const roundedNumbers = numbers.map((number) => {
-//       return Number(number.toFixed(2));
-//     });
-
-//     console.log(`Ejemplo ${roundedNumbers}`); // Output: [1.23, 2.35, 3.46, 4.57]
-//   }
-// }
