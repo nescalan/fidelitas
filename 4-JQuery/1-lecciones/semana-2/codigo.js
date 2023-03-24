393be6ef2b57635a8286ec42baa1b74cfae718d5
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-03-23 20:00:24 -0500

    update

---
+++ 4-JQuery/1-lecciones/semana-2/codigo.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $("p")
+    .css("color", "rgb(0,50,0)")
+    .css("font-size", "60px")
+    .css("background-color", "green")
+    .css("text-align", "center");
+
+  $("#titulo").css("color", "rgb(80,0,0");
+  $("#nombre").attr("placeholder", "Ingrese su nombre");
+  $("#clave").attr("placeholder", "Password");
+});
