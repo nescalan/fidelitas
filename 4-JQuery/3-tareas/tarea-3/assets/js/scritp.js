b294d504c10bf90077836726c3fe66032eaa72e2
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-04-08 17:33:32 -0600

    Added new functions to render tasks and clear unused DOM elements
    I have added two new functions to the project that will help with rendering tasks and managing DOM elements.
    
    The first function is called renderTasks(), which takes an array of tasks as input and renders them into the DOM. This function loops through each task in the array, creates a new HTML element for it, and appends it to the appropriate section in the DOM. This will make it easier to display all the tasks in the application and keep them organized.
    
    The second function is called clearUnusedElements(), which clears any DOM elements that are no longer in use. This function takes an array of elements that should be kept and removes all other elements from the DOM. This will help keep the DOM clean and prevent memory leaks, which can slow down the application over time.
    
    Together, these two functions will help make the application more efficient and easier to use by keeping the DOM organized and free from unnecessary elements.

--- 4-JQuery/3-tareas/tarea-3/assets/js/scritp.js
+++ 4-JQuery/3-tareas/tarea-3/assets/js/scritp.js
@@ -1,20 +1,84 @@
 $(document).ready(function () {
+  // FUNCTIONS
+  function renderTasks() {
+    tasksList.map((task) => {
+      const domAddedTask = $("#added-task");
+      const groupedTasks = `
+        <div class="grouped-tasks">
+          <input id="checkbox" type="checkbox" name="checkbox" onclick />
+          <div id="task-description" class="task-description">${task.description}</div>
+        </div>
+      `;
+      if (task.completed == false) {
+        domAddedTask.append(groupedTasks);
+      }
+    });
+  }
+
+  function clearUnusedElements() {
+    const domAddedTask = $("#added-task");
+    domAddedTask.empty();
+  }
+
+  // LEFT COLUMN **************************************************
+  renderTasks();
+  // Set the number of tasks on left column
+  let tasksListLength = tasksList.length;
+  $("#all-tasks").text(tasksListLength);
+
+  // Select if the task is pending or not
+  let pendingTasks = tasksList.filter((task) => !task.completed).length;
+  $("#pending-tasks").text(pendingTasks);
+
+  // Select if the task is pending or not
+  const completedTasks = tasksList.filter((task) => task.completed).length;
+  $("#completed-tasks").text(completedTasks);
+
+  // RIGHT COLUMN **************************************************
+  // RENDER TASK ITEM INTO THE DOM WITH MAP FUNCTION
+
+  // ADD NEW TASK INTO THE ARRAY
   //Bind keypress event to input
   $("#new-task").keypress(function (event) {
     var keycode = event.keyCode ? event.keyCode : event.which;
     if (keycode == "13") {
-      alert('You pressed a "enter" key in the input text');
-
       //   CAPTURA NUEVA TAREA
       let domNewTask = $("#new-task").val();
       let domAddedTask = $("#added-task");
-
-      alert(domNewTask);
       domAddedTask.append(domNewTask);
     }
     //Stop the event from propogation to other handlers
-    //If this line will be removed, then keypress event handler attached
-    //at document level will also be triggered
     event.stopPropagation();
   });
+
+  // MARK TASK AS COMPLETED
+  $("#checkbox").change(function () {
+    const domTaskDescription = $("#task-description").text();
+    const taskIndex = tasksList.findIndex(
+      (task) => task.description == domTaskDescription
+    );
+    tasksList[taskIndex].completed = true;
+    // alert(taskIndex);
+    clearUnusedElements();
+    renderTasks();
+  });
+
+  // COMPLETE A TASK
+  function completeTask(text) {
+    const taskIndex = tasksList.findIndex((task) => task.description == text);
+    tasksList[taskIndex].completed = true;
+  }
+
+  // ************************************************************************
+
+  $(document)
+    //Creamos la Funcion del Click
+    .click(function () {
+      //Creamos una Variable y Obtenemos el Numero de Checkbox que esten Seleccionados
+      var checked = $("#checkbox:checked").length;
+      $("p").text(
+        "Tienes Actualmente " + checked + " Checkbox " + "Seleccionado(s)"
+      ); //Asignamos a la Etiqueta <p> el texto de cuantos Checkbox ahi Seleccionados(Combinando la Variable)
+    })
+    .trigger("click"); //Simulamos el Evento Click(Desde el Principio, para que muestre cuantos ahi Seleccionados)
 });
---
+++ 4-JQuery/3-tareas/tarea-3/assets/js/tasks.js
@@ -0,0 +1,18 @@
+let tasksList = [
+  {
+    description: "Trabajo para el domingo",
+    completed: false,
+  },
+  {
+    description: "Almorzar con Marco y Nathy",
+    completed: false,
+  },
+  {
+    description: "El lunes es feriado",
+    completed: false,
+  },
+  {
+    description: "Practicar escalas pentatonicas",
+    completed: false,
+  },
+];
