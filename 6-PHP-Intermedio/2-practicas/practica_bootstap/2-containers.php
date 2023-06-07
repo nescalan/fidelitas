788b45cb9aa69cf8287bdf4d7747caaf7c3195fd
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-06-06 20:55:18 -0600

    Update

---
+++ 6-PHP-Intermedio/2-practicas/practica_bootstap/2-containers.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Bootstrap | Get Started</title>
+</head>
+
+<body>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <!-- Latest compiled and minified CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <title>Bootstrap | Get Started</title>
+    </head>
+
+    <body>
+
+        <div class="container mt-3 p-5 border">
+            <h1>My First Bootstrap Page</h1>
+            <p>This part is inside a .container class.</p>
+            <p>The .container class provides a responsive fixed width container.</p>
+        </div>
+
+        <div class="container mt-3 p-5 border bg-dark text-white">
+            <h1>My First Bootstrap Page</h1>
+            <p>This part is inside a .container class.</p>
+            <p>The .container class provides a responsive fixed width container.</p>
+        </div>
+
+        <div class="container mt-3 p-5 border bg-primary text-white">
+            <h1>My First Bootstrap Page</h1>
+            <p>This part is inside a .container class.</p>
+            <p>The .container class provides a responsive fixed width container.</p>
+        </div>
+
+        <div class="container-sm mt-3 bg-primary text-white">.container-sm</div>
+        <div class="container-md mt-3 bg-primary text-white">.container-md</div>
+        <div class="container-lg mt-3 bg-primary text-white">.container-lg</div>
+        <div class="container-xl mt-3 bg-primary text-white">.container-xl</div>
+        <div class="container-xxl mt-3 bg-primary text-white">.container-xxl</div>
+
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+    </html>
+
+
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
