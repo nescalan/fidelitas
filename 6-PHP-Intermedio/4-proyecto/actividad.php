05415a1a55acf29c39f7b70ca5356488eb37f557
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-06-08 11:59:47 -0600

    FEAT: Logo Size
    Logo new dimensions: width="32" height="32"

--- 6-PHP-Intermedio/4-proyecto/actividad.php
+++ 6-PHP-Intermedio/4-proyecto/actividad.php
@@ -1,5 +1,13 @@
 <?php #actividad.php 
 
+require_once "./src/model/connection-db.model.php";
 require_once "./src/views/actividad.view.php";
+
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+echo "Connected successfully";
 
 ?>
