9e6572d84e78d8ff0d7f0ac4971467819df1cc89
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-06-08 14:51:20 -0600

    Vendor folder, needed for pagination

--- 6-PHP-Intermedio/4-proyecto/src/views/inquilinos.view.php
+++ 6-PHP-Intermedio/4-proyecto/src/views/inquilinos.view.php
@@ -35,10 +35,9 @@
                         <th scope="col">Estado</th>
                     </tr>
                 </thead>
+
                 <tbody>
-
                     <?php
-
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo '
                         <tr>
@@ -52,6 +51,15 @@
                     ?>
                 </tbody>
             </table>
+            <nav aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
 
         </div>
     </main>
