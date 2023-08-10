748edf75422682c59cbfbd988d86f26efc3ad5b0
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-08-10 11:56:32 -0600

    Update

--- 7-PHP-avanzado/4-proyecto/app/model/database_model/1-create/create_table_publications.sql
+++ 7-PHP-avanzado/4-proyecto/app/model/database_model/1-create/create_table_publications.sql
@@ -1,12 +1,17 @@
--- -----------------------------------------------------
--- Table `mydb`.`publications`
--- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `blog`.`publications` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(200) NULL,
-  `summary` VARCHAR(200) NULL,
-  `date` TIMESTAMP NULL,
-  `post_content` TEXT NULL,
-  `thumb` VARCHAR(50) NULL,
-  PRIMARY KEY (`id`)
-  ) ENGINE = InnoDB;
+  `title` VARCHAR(200) NULL DEFAULT NULL,
+  `summary` VARCHAR(200) NULL DEFAULT NULL,
+  `date` TIMESTAMP NULL DEFAULT NULL,
+  `post_content` TEXT NULL DEFAULT NULL,
+  `thumb` VARCHAR(50) NULL DEFAULT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_publications_id_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_user_publications_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `blog`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3
