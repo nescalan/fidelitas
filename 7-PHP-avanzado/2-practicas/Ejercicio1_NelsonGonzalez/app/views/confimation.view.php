96a8c0c23f0303e7f0e19d437c3b0d9947727255
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-08-15 13:46:15 -0600

    FEAT: new views
    + Confirmation view
    + Message view

---
+++ 7-PHP-avanzado/2-practicas/Ejercicio1_NelsonGonzalez/app/views/confimation.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- STYLES: Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Ejercicio 1 - Nelson Gonzalez</title>
+</head>
+
+<body class="p-4">
+
+    <section class="container mb-4 border shadow p-5 mt-5 bg-white rounded">
+        <p>
+            El correo se envió con éxito.
+        </p>
+        <p>
+            Si desea enviar otro correo, pulsa el siguiente botón.
+        </p>
+        <a class="btn btn-primary" href="./index.php">Regresar</a>
+    </section>
+
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"></script>
+</body>
+
+</html>
