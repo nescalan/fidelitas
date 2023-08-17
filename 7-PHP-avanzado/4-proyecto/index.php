cdabd7ce20065ad3b431f2fc43affd71ed46b52b
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-08-16 20:26:46 -0600

    Update

--- 7-PHP-avanzado/4-proyecto/index.php
+++ 7-PHP-avanzado/4-proyecto/index.php
@@ -1,13 +1,11 @@
 <?php //index.php
 
 # Database Connections file
-require_once './app/model/db/Connection.model.php';
+require_once './app/model/db_connection/Connection.model.php';
 # Configuration file
 require_once './app/admin/config.php';
-
 # Functions file
 require_once './app/funcs/functions.php';
-
 # Call index view
 require_once './app/views/index.view.php';
 
