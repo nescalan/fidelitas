6b2b77f79736bce400d043d511550e0f41bcf150
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-08-17 00:18:06 -0600

    feat: Add dynamic content retrieval and rendering in index.php
    This commit introduces dynamic content retrieval and rendering in the `index.php` file. The following changes have been made:
    
    - Imported `Connection.model.php` to establish a database connection.
    - Included the `config.php` file for configuration settings.
    - Utilized `functions.php` to retrieve publications based on configuration.
    - Initialized a new database connection using `Connection` class and configuration data.
    - Implemented a try-catch block to handle potential exceptions during the process.
    
    Inside the `try` block:
    - Opened a database connection using `openConnection()` method.
    - Retrieved publications using `getPublications()` function with the specified limit.
    - Checked if publications were retrieved successfully; if not, redirected to the error page.
    - Iterated through the retrieved posts and their values to echo them.
    
    Additionally:
    - Included the `index.view.php` to render the index view.
    - Closed the database connection using `closeConnection()` method.
    
    In case of any exceptions, the catch block handles them and displays an error message.
    
    This commit enhances the `index.php` file by dynamically fetching and displaying content from the database, improving the functionality of the application.

--- 7-PHP-avanzado/4-proyecto/app/model/db_connection/Connection.model.php
+++ 7-PHP-avanzado/4-proyecto/app/model/db_connection/Connection.model.php
@@ -3,27 +3,36 @@
 class Connection
 {
     # Properties 
-    protected $host = 'localhost'; // Host name or IP address of the database server
-    protected $user = 'root'; // Username to access the database with read/write privileges
-    protected $password = '4u3p7px6'; // Password for that user account
-    protected $db = 'blog'; // Name of the database we are connecting to
+    protected $host; // Host name or IP address of the database server
+    protected $user; // Username to access the database with read/write privileges
+    protected $password; // Password for that user account
+    protected $db; // Name of the database we are connecting to
+
+    # Constructor Method
+    public function __construct($host, $user, $password, $db)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->db = $db;
+    }
 
     # Create connection
     public function openConnection()
     {
         $mysqli = mysqli_connect($this->host, $this->user, $this->password, $this->db);
 
-        if (!$mysqli) {
-            die("Connection failed: " . mysqli_connect_error());
+        if ($mysqli->connect_error) {
+            throw new Exception("Connection failed: " . $mysqli->connect_error);
         }
 
         return $mysqli;
     }
 
     # Close connection
-    public function closeConnection($mysqli)
+    public function closeConnection($pDBConnection)
     {
-        mysqli_close($mysqli);
+        mysqli_close($pDBConnection);
     }
 
 
--- 7-PHP-avanzado/4-proyecto/app/views/index.view.php
+++ 7-PHP-avanzado/4-proyecto/app/views/index.view.php
@@ -43,47 +43,37 @@
   <?php require 'app/views/header.view.php'; ?>
 
   <section class="u-align-center u-clearfix u-grey-5 u-section-1" id="carousel_9e24">
-    <div class="u-clearfix u-sheet u-sheet-1">
-      <img class="u-expanded-width u-image u-image-default u-image-1" src="./public/img/2.png" alt=""
-        data-image-width="1620" data-image-height="1080">
-      <div
-        class="u-align-left u-border-16 u-border-no-bottom u-border-no-right u-border-no-top u-border-palette-1-base u-container-style u-expanded-width u-group u-white u-group-1"
-        data-animation-name="customAnimationIn" data-animation-duration="1500">
-        <div class="u-container-layout u-valign-middle u-container-layout-1">
-          <h5 class="u-text u-text-1">6 de agosto de 2023</h5>
-          <h2 class="u-custom-font u-font-montserrat u-text u-text-default u-text-2">
-            <a href="#">How to Install PHP 8 on Ubuntu 20.04</a>
-          </h2>
-          <p class="u-text u-text-3">
-            PHP is arguably one of the most widely used server-side programming languages.
-          </p>
-          <a href="#"
-            class="u-active-palette-1-base u-border-3 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-palette-1-base u-btn u-button-style u-hover-palette-1-base u-none u-text-active-white u-text-hover-white u-text-palette-1-base u-btn-1">continuar
-            leyendo</a>
-        </div>
-      </div>
-    </div>
 
-    <!-- NEW POST -->
-    <div class="u-clearfix u-sheet u-sheet-1">
-      <img class="u-expanded-width u-image u-image-default u-image-1" src="./public/img/1.png" alt=""
-        data-image-width="1620" data-image-height="1080">
-      <div
-        class="u-align-left u-border-16 u-border-no-bottom u-border-no-right u-border-no-top u-border-palette-1-base u-container-style u-expanded-width u-group u-white u-group-1"
-        data-animation-name="customAnimationIn" data-animation-duration="1500">
-        <div class="u-container-layout u-valign-middle u-container-layout-1">
-          <h5 class="u-text u-text-1">7 de julio de 2023</h5>
-          <h2 class="u-custom-font u-font-montserrat u-text u-text-default u-text-2"><a href="#">Growth Strategy</a>
-          </h2>
-          <p class="u-text u-text-3"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.</p>
-          <a href="#"
-            class="u-active-palette-1-base u-border-3 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-palette-1-base u-btn u-button-style u-hover-palette-1-base u-none u-text-active-white u-text-hover-white u-text-palette-1-base u-btn-1">continuar
-            leyendo</a>
-        </div>
-      </div>
-    </div>
+    <!-- Paint post into the DOM -->
+    <?php
+    while ($row = mysqli_fetch_assoc($posts)) {
+      // Get the email address from the database.
+      $email_address = $row["correo"];
+      echo '
+            <div class="u-clearfix u-sheet u-sheet-1">
+            <img class="u-expanded-width u-image u-image-default u-image-1" src="./public/img/' . $row["thumb"] . '" alt=""
+              data-image-width="1620" data-image-height="1080">
+
+            <div
+              class="u-align-left u-border-16 u-border-no-bottom u-border-no-right u-border-no-top u-border-palette-1-base u-container-style u-expanded-width u-group u-white u-group-1"
+              data-animation-name="customAnimationIn" data-animation-duration="1500">
+              <div class="u-container-layout u-valign-middle u-container-layout-1">
+                <h5 class="u-text u-text-1"> ' . $row["date"] . ' </h5>
+                <h2 class="u-custom-font u-font-montserrat u-text u-text-default u-text-2">
+                  <a href="#"> ' . $row["title"] . ' </a>
+                </h2>
+                <p class="u-text u-text-3"> ' . $row["summary"] . ' </p>
+                <a href="#"
+                  class="u-active-palette-1-base u-border-3 u-border-active-palette-1-base u-border-hover-palette-1-base u-border-palette-1-base u-btn u-button-style u-hover-palette-1-base u-none u-text-active-white u-text-hover-white u-text-palette-1-base u-btn-1">continuar
+                  leyendo</a>
+              </div>
+            </div>
+          </div>
+        ';
+    }
+    ?>
+
+    <?php require 'paginacion.php'; ?>
     <?php require './app/views/pagination.view.php'; ?>
   </section>
 
