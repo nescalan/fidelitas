2d407eb53a5824c8485947153c08e9a6be4fc2b2
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-08-22 11:07:45 -0600

    Update

--- 7-PHP-avanzado/3-tareas/app/views/components/Menu.php
+++ 7-PHP-avanzado/3-tareas/app/views/components/Menu.php
@@ -1,17 +1,40 @@
 <?php
 
-$Menu = '
+
+$sessionVar = [
+  'id' => $_SESSION["identificacion"],
+  'name' => $_SESSION["Nombre"],
+  'rol' => $_SESSION['Rol'],
+];
+
+
+if ($sessionVar['rol'] != 1) {
+  echo "No es administrador";
+} else {
+  echo "Bienvenido super usuario ";
+}
+
+
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">PHP Avanzado</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="home.php">PHP Avanzado</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Clientes.php">Clientes</a>
-        </li>
+
+        <?php if ($sessionVar['rol'] != 1): ?>
+          echo "No es administrador";
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="clientes.php">Clientes</a>
+          </li>
+        <?php endif; ?>
+
         <li class="nav-item">
           <a class="nav-link" href="#">Funciones</a>
         </li>
@@ -25,8 +48,3 @@ $Menu = '
     </div>
   </div>
 </nav>
-';
-
-echo $Menu;
-
-?>
