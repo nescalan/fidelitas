db45ccf49adc54d98f7fd694b6b63390b413288f
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-08-23 16:24:32 -0600

    Refactor: Improve search functionality and database connection
    In this commit, several improvements and changes have been made to the PHP code:
    
    1. Database Connection: The code now uses the 'Connection' class from the 'db_connection' directory to establish a database connection. The connection details are retrieved from the 'config.php' file, making it easier to manage database configurations.
    
    2. Sanitization: User input from the 'search' parameter is now sanitized using the 'sanitizeData' function to prevent potential security vulnerabilities.
    
    3. Improved SQL Query: The SQL query for retrieving publications has been enhanced to use parameterized queries for better security against SQL injection. It now searches for matches in both the 'title' and 'summary' fields of the 'publications' table.
    
    4. Error Handling: Proper error handling has been implemented. If the database connection fails or an exception is thrown, the code redirects to the 'error.php' page.
    
    5. Result Handling: The code checks if the result set is empty and sets the 'titulo' variable accordingly, providing a message to display in the view.
    
    6. Redirection: If the 'search' parameter is missing or the HTTP request method is not 'GET', the code redirects to the 'index.php' page.
    
    7. Separation of Concerns: The code structure has been organized, separating database connections, configurations, and functions into their respective files for better maintainability.
    
    These changes improve code readability, security, and maintainability. This commit enhances the search functionality and ensures proper error handling in case of issues with the database connection or other exceptions.

--- 7-PHP-avanzado/4-proyecto/app/views/pagination.view.php
+++ 7-PHP-avanzado/4-proyecto/app/views/pagination.view.php
@@ -1,15 +1,36 @@
-<?php
-
-$pagesNumber = pagesNumber($blog_config['publication_per_page'], $dbConnection);
-echo " Numero de paginas: $pagesNumber";
-
-?>
 <section class="paginacion">
     <ul>
-        <li class="disabled">&laquo;</li>
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">&raquo;</a></li>
+        <!-- Set the number of pages -->
+        <?php
+        $pagesNumber = pagesNumber($blog_config['publication_per_page'], $dbConnection);
+        echo " Numero de paginas: $pagesNumber";
+        ?>
+        <!-- Show the button to go back one page -->
+        <?php if (actualPage() === 1): ?>
+            <li class="disabled">&laquo;</li>
+        <?php else: ?>
+            <li><a href="index.php?p=<?php echo actualPage() - 1 ?>">&laquo;</a></li>
+        <?php endif; ?>
+
+        <!-- Create a <li> element for each page we have -->
+        <?php for ($i = 1; $i <= $pagesNumber; $i++): ?>
+            <!-- Add the active class on the current page  -->
+            <?php if (actualPage() === $i): ?>
+                <li class="active">
+                    <?php echo $i; ?>
+                </li>
+            <?php else: ?>
+                <li>
+                    <a href="index.php?p=<?php echo $i ?>"><?php echo $i; ?></a>
+                </li>
+            <?php endif; ?>
+        <?php endfor; ?>
+
+        <!-- Show the button to go forward one page -->
+        <?php if (actualPage() == $pagesNumber): ?>
+            <li class="disabled">&raquo;</li>
+        <?php else: ?>
+            <li><a href="index.php?p=<?php echo actualPage() + 1; ?>">&raquo;</a></li>
+        <?php endif; ?>
     </ul>
 </section>
