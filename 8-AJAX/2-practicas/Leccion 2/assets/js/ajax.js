8fb2b320157242e5672fa9b90652a57e71dcfc12
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-10-19 22:36:31 -0600

    Actualizacion del funcionamiento

--- 8-AJAX/2-practicas/Leccion 2/assets/js/ajax.js
+++ 8-AJAX/2-practicas/Leccion 2/assets/js/ajax.js
@@ -1,24 +1,3 @@
-function pruebaAjax() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    console.log(this.responseText);
-    alert(this.responseText);
-  };
-
-  xhttp.open("GET", "txt/prueba.txt");
-  xhttp.send();
-}
-
-function cargar(texto) {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    console.log(this.responseText);
-    document.getElementById("img").innerHTML = this.responseText;
-  };
-  xhttp.open("GET", "txt/" + texto + ".txt");
-  xhttp.send();
-}
-
 function cargarApi() {
   const txtCedula = document.getElementById("txtCed").value;
   const xhttp = new XMLHttpRequest();
@@ -26,19 +5,51 @@ function cargarApi() {
   xhttp.onload = function () {
     console.log(this.responseText);
 
-    const response = JSON.parse(this.responseText);
-    console.log(response.nombre);
-    document.getElementById("nombre").value = response.nombre;
-    document.getElementById("moroso").value = response.situacion.moroso;
+    const inputElement = document.getElementById("txtCed").value;
+    if (!inputElement) {
+      const errorMessage = `
+        <div class="alert alert-danger" role="alert">
+          <strong>Error!</strong> Debe ingresar una identificación.
+        </div>`;
+      document.getElementById("message").innerHTML = errorMessage;
 
-    if (response.actividades.length === 0) {
-      console.log("El arreglo está vacío");
-      const actividades = "No hay datos disponibles";
-      document.getElementById("descripcion").value = actividades;
+      // Clear the 'nombre' input
+      const nombreInput = document.getElementById("nombre");
+      nombreInput.removeAttribute("disabled");
+      nombreInput.value = "";
+      nombreInput.setAttribute("disabled", true);
+
+      // Clear the 'moroso' input
+      const morosoInput = document.getElementById("moroso");
+      morosoInput.removeAttribute("disabled");
+      morosoInput.value = "";
+      morosoInput.setAttribute("disabled", true);
+
+      // Clear the 'descripcion' textarea
+      const descripcionTextarea = document.getElementById("descripcion");
+      descripcionTextarea.removeAttribute("disabled");
+      descripcionTextarea.value = "";
+      descripcionTextarea.setAttribute("disabled", true);
     } else {
-      console.log("El arreglo no está vacío");
-      const actividades = `Estado: ${response.actividades[0].estado}\nTipo: ${response.actividades[0].tipo}\nCódigo: ${response.actividades[0].codigo}\nDescripción: ${response.actividades[0].descripcion} `;
-      document.getElementById("descripcion").value = actividades;
+      const response = JSON.parse(this.responseText);
+      const successMessage = `
+        <div class="alert alert-success" role="alert">
+        ¡La consulta se realizó <strong>correctamente!</strong>
+        </div>`;
+      console.log(response.nombre);
+      document.getElementById("message").innerHTML = successMessage;
+      document.getElementById("nombre").value = response.nombre;
+      document.getElementById("moroso").value = response.situacion.moroso;
+
+      if (response.actividades.length === 0) {
+        console.log("El arreglo está vacío");
+        const actividades = "No hay datos disponibles";
+        document.getElementById("descripcion").value = actividades;
+      } else {
+        console.log("El arreglo no está vacío");
+        const actividades = `Estado: ${response.actividades[0].estado}\nTipo: ${response.actividades[0].tipo}\nCódigo: ${response.actividades[0].codigo}\nDescripción: ${response.actividades[0].descripcion} `;
+        document.getElementById("descripcion").value = actividades;
+      }
     }
   };
 
