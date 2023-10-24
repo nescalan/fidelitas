cdea68721d0cb9b37e6ff5f9600026cb504f44a8
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-10-24 16:53:13 -0600

    Update

--- 8-AJAX/2-practicas/ajax/assets/js/xmlhttprequest.js
+++ 8-AJAX/2-practicas/ajax/assets/js/xmlhttprequest.js
@@ -3,3 +3,12 @@ let client = new XMLHttpRequest();
 client.open("GET", "assets/txt/generated.json"); // Preparamos la petición
 client.send(); // Enviamos la consulta
 console.log(client.responseText); // Consultamos el contenido de la respuesta
+
+function getPokemon() {
+  let xhttp = new XMLHttpRequest();
+  const API = "https://pokeapi.co/api/v2/pokemon";
+
+  xhttp.onload = () => {
+    console.log(xhttp.responseText);
+  };
+}
