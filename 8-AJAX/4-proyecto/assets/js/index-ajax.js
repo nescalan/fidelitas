6218a1119ee9a28985e13bceb87dcae9c58fbe93
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-10-26 20:50:29 -0600

    Update

--- 8-AJAX/4-proyecto/assets/js/index-ajax.js
+++ 8-AJAX/4-proyecto/assets/js/index-ajax.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Captura del boton "buscar"
   const btnBuscar = document.getElementById("btn-buscar");
+  const inputElement = document.getElementById("buscar");
+
+  // Captura el momento en que pulsan la tecla "enter"
+  inputElement.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.keyCode === 13) {
+      let txtPokemon = document.getElementById("buscar").value;
+      txtPokemon = txtPokemon.toLowerCase();
+
+      const validatedInputText = validateInputText(txtPokemon);
+      inputElement.value = "";
+
+      // CONDICIONAL:
+      if (validatedInputText) {
+        callApi(txtPokemon);
+      }
+    }
+  });
 
   // Captura la acción del botón buscar
   btnBuscar.addEventListener("click", function () {
@@ -8,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     txtPokemon = txtPokemon.toLowerCase();
 
     const validatedInputText = validateInputText(txtPokemon);
+    inputElement.value = "";
 
     // CONDICIONAL:
     if (validatedInputText) {
