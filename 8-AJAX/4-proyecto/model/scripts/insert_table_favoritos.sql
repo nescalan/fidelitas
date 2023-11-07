c40d2f4c353799a68b2c2185ea0f9a98c809c094
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-11-07 09:38:17 -0600

    Create insert table favoritos

---
+++ 8-AJAX/4-proyecto/model/scripts/insert_table_favoritos.sql
@@ -0,0 +1,13 @@
+-- Insert ten Pokémon characters into the `favorites` table
+INSERT INTO `pokemon`.`favorites` (`id`, `name`, `type`, `ability`, `attack`, `speed`)
+VALUES
+(1, 'Pikachu', 'Electric', 'Static', '55', '90'),
+(2, 'Charizard', 'Fire/Flying', 'Blaze', '84', '100'),
+(3, 'Bulbasaur', 'Grass/Poison', 'Overgrow', '49', '45'),
+(4, 'Mewtwo', 'Psychic', 'Pressure', '110', '130'),
+(5, 'Gyarados', 'Water/Flying', 'Intimidate', '125', '81'),
+(6, 'Snorlax', 'Normal', 'Immunity', '110', '30'),
+(7, 'Gengar', 'Ghost/Poison', 'Levitate', '65', '110'),
+(8, 'Machamp', 'Fighting', 'Guts', '130', '55'),
+(9, 'Eevee', 'Normal', 'Adaptability', '55', '55'),
+(10, 'Lugia', 'Psychic/Flying', 'Pressure', '90', '110');
