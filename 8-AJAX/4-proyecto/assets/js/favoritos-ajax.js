5182417ff169d8e02768f752aa92a3519787a9cf
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-11-07 13:31:59 -0600

    FIX: Mejora del código existente
    Descripción
    Este pull request tiene como objetivo mejorar el código existente en el archivo JavaScript. El código actual se encarga de cargar datos desde un servidor y mostrarlos en una tabla HTML cuando el DOM se carga por completo. Las mejoras propuestas incluyen una mejor organización del código, comentarios más descriptivos y algunas correcciones menores.
    
    Cambios Propuestos
    1. Organización y Comentarios
    Se ha realizado una reorganización del código para mejorar la legibilidad y el entendimiento. Se han añadido comentarios descriptivos para cada sección importante del código.
    
    2. Comentarios Adicionales
    Se han agregado comentarios descriptivos en las secciones clave del código para explicar su propósito y funcionamiento. Esto mejorará la comprensión del código para futuros desarrolladores que puedan revisarlo.
    
    3. Documentación
    Se recomienda proporcionar documentación adicional sobre la función de este código, incluyendo detalles sobre la estructura esperada de los datos JSON y la estructura de la tabla HTML que se espera en el DOM.
    
    Pruebas
    Se recomienda realizar pruebas exhaustivas en diferentes entornos para garantizar que la solicitud HTTP se realiza correctamente y que los datos se muestran correctamente en la tabla.
    
    Impacto
    Estos cambios no deberían tener ningún impacto negativo en la funcionalidad existente, pero mejorarán la legibilidad y el mantenimiento del código.Revisores Sugeridos
    [Lista de revisores sugeridos]
    Tareas Adicionales
    [Lista de tareas adicionales, si es necesario]
    Notas
    [Notas adicionales, si es necesario]
    Estos cambios mejoran la calidad y mantenibilidad del código existente. Gracias por considerar este pull request.

--- 8-AJAX/4-proyecto/assets/js/favoritos-ajax.js
+++ 8-AJAX/4-proyecto/assets/js/favoritos-ajax.js
@@ -1,22 +1,39 @@
+// Se espera a que el DOM se cargue antes de ejecutar el código
 document.addEventListener("DOMContentLoaded", () => {
-  // Cargando valores del DOM en variables
-  let tblId = document.getElementById("tbl-id"),
-    tblName = document.getElementById("tbl-name"),
-    tblType = document.getElementById("tbl-type"),
-    tblAbility = document.getElementById("tbl-ability"),
-    tblAttack = document.getElementById("tbl-attack"),
-    tblSpeed = document.getElementById("tbl-speed");
-
-  const xhttp = new XMLHttpRequest();
+  // Obteniendo elementos del DOM
+  let tblBody = document.getElementById("tbl-body");
+  let xhttp = new XMLHttpRequest();
   xhttp.open("GET", "assets/php/stringToJson.php");
-  xhttp.onload = function () {
-    const response = JSON.parse(this.responseText);
-    // console.log(response);
 
-    response.forEach((row) => {
-      console.table(row);
-    });
+  // Definición de la función que se ejecuta cuando la solicitud se completa
+  xhttp.onload = function () {
+    if (xhttp.status === 200) {
+      // Verificar que la solicitud se realizó con éxito
+
+      // Parsear la respuesta como un objeto JSON
+      const response = JSON.parse(this.responseText);
+      let newRow = "";
+
+      // Iterar sobre los objetos en la respuesta JSON
+      response.forEach((row) => {
+        // Crear una fila de tabla con los datos de cada objeto
+        newRow += `
+          <tr>
+            <th scope="row">${row.id}</th>
+            <td scope="col">${row.name}</td>
+            <td scope="col">${row.type}</td>
+            <td scope="col">${row.ability}</td>
+            <td scope="col">${row.attack}</td>
+            <td scope="col">${row.speed}</td>
+          </tr>
+        `;
+      });
+
+      // Agregar las filas a la tabla en el DOM
+      tblBody.innerHTML = newRow;
+    }
   };
 
+  // Enviar la solicitud al servidor
   xhttp.send();
 });
