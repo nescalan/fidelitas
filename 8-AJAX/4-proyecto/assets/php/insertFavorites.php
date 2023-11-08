3ab62d76602031731017df0ac8ff1ecd87a226ff
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-11-08 15:30:25 -0600

    Update

--- 8-AJAX/4-proyecto/assets/php/insertFavorites.php
+++ 8-AJAX/4-proyecto/assets/php/insertFavorites.php
@@ -7,33 +7,47 @@ $ability = $_POST['ability'];
 $attack = $_POST['attack'];
 $speed = $_POST['speed'];
 
-
-
 // Establece la conexión con la base de datos.
 $conexion = new mysqli('localhost', 'root', '4u3p7px6', 'pokemon');
 $conexion->set_charset("utf8");
 
 // Verifica si hay un error de conexión.
 if ($conexion->connect_errno) {
-    $inserFavorite = [
-        'error' => true
-    ];
+    $respuesta = "Error en la conexión a la base de datos";
 } else {
-    // Prepara la consulta de inserción SQL.
-    $insertFavorite = "INSERT INTO favorites (id, name, type, ability, attack, speed)
-    VALUES ('$id', '$name', '$type', '$ability', '$attack', '$speed')";
+    // Verificar si el ID ya existe en la tabla "favorites".
+    $idExistQuery = "SELECT id FROM favorites WHERE id = '$id'";
+    $result = $conexion->query($idExistQuery);
 
-    // Verifica si la consulta se ejecutó correctamente.
-    if (mysqli_query($conexion, $insertFavorite)) {
-        echo "New record created successfully";
-        $respuesta = true;
+    if ($result->num_rows > 0) {
+        // $respuesta = "El ID ya existe en la base de datos";
+        $respuesta = "   
+        <div class='alert alert-danger mt-3 text-center' role='alert'>
+        <strong>Error!</strong> <br />The Pokémon already exists in the favorites list
+        </div>";
     } else {
-        $respuesta = false;
-    }
+        // Prepara la consulta de inserción SQL.
+        $insertFavorite = "INSERT INTO favorites (id, name, type, ability, attack, speed)
+        VALUES ('$id', '$name', '$type', '$ability', '$attack', '$speed')";
 
+        // Verifica si la consulta se ejecutó correctamente.
+        if ($conexion->query($insertFavorite) === TRUE) {
+            $respuesta = "   
+            <div class='alert alert-success mt-3 text-center' role='alert'>
+            <strong>Success!</strong> <br />Record inserted successfully
+            </div>";
+        } else {
+            $respuesta = "   
+            <div class='alert alert-danger mt-3 text-center' role='alert'>
+            <strong>Error!</strong> <br />Cannot insert record
+            </div>";
+        }
+    }
 }
 
 // Cierra la conexión a la base de datos.
-mysqli_close($conexion);
+$conexion->close();
 
+// Devuelve la respuesta al cliente.
+echo $respuesta;
 ?>
